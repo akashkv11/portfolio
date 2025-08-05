1774e9e277906d44c7d70a2ba66d94fccaa3d983
akashk11 <akashvinod777@gmail.com>
2025-08-05 11:24:47 +0400

    Fix particles.js config path in main.ts

--- src/main.ts
+++ src/main.ts
@@ -88,7 +88,7 @@ if (form) {
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load(
   "particles-js",
-  "/public/assets/particlesjs-config.json",
+  "/assets/particlesjs-config.json",
   function () {
     console.log("callback - particles.js config loaded");
   }
